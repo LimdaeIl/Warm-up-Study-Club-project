15b5dbe1ae62de4e4c78967c1977c56d1c0d6700
LimDae94 <piay801@naver.com>
2024-03-07 23:47:54 +0900

    #16 - 멤버 리포지토리에도 `@Query` 적용하기
    출근한 모든 직원에 대해 `DISDICT` 옵션을 부여하고 어느 직원이 해당 월에 출근했는지 찾고 이름을 반환합니다.

--- corporation/src/main/java/inflearn/com/corporation/member/repository/MemberRepository.java
+++ corporation/src/main/java/inflearn/com/corporation/member/repository/MemberRepository.java
@@ -2,9 +2,13 @@ package inflearn.com.corporation.member.repository;
 
 import inflearn.com.corporation.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberById(Long memberId);
+
+    @Query("SELECT m.name FROM Member m WHERE m.id = :id")
+    String findNameById(Long id);
 }
